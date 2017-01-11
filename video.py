c2a23223fd62831054f59fbae3c66cb00cae33d0
Mikel Larreategi <mlarreategi@codesyntax.com>
2017-01-11 16:43:51 +0100

    target jarvis
    set mediatype for episodes
    
    setContent for episodes
    
    add mediatype info

--- video.py
+++ video.py
@@ -22,11 +22,13 @@ class VideoHandler(object):
             program_url = program.get('@id')
             # Create a list item with a text label and a thumbnail image.
             list_item = xbmcgui.ListItem(label=title)
-            list_item.setInfo('video', {'title': title})
+            list_item.setInfo('video', {'title': title, 'mediatype': 'tvshow'})
             url = '{0}?action=videolisting&program={1}'.format(self.url, program_url)
             is_folder = True
             listing.append((url, list_item, is_folder))
 
+        # Set Content
+        xbmcplugin.setContent(self.handle, 'tvshows')
         # Add our listing to Kodi.
         xbmcplugin.addDirectoryItems(self.handle, listing, len(listing))
         # Add a sort method for the virtual folder items
@@ -56,11 +58,13 @@ class VideoHandler(object):
             list_item.setArt({'thumb': episode.get('episode_image_thumbnail'),
                               'fanart': episode.get('episode_image')})
 
-            list_item.setInfo('video', {'title': title, 'plot': desc})
+            list_item.setInfo('video', {'title': title, 'plot': desc, 'mediatype': 'episode'})
             url = '{0}?action=videoepisode&program={1}'.format(self.url, episode_url)
             is_folder = True
             listing.append((url, list_item, is_folder))
 
+        # Set Content
+        xbmcplugin.setContent(self.handle, 'episodes')
         # Add our listing to Kodi.
         xbmcplugin.addDirectoryItems(self.handle, listing, len(listing))
         # Add a sort method for the virtual folder items
@@ -79,7 +83,7 @@ class VideoHandler(object):
                 # Create a list item with a text label and a thumbnail image.
                 list_item = xbmcgui.ListItem(label=title)
                 # Set additional info for the list item.
-                list_item.setInfo('video', {'title': title})
+                list_item.setInfo('video', {'title': title, 'mediatype': 'video'})
                 # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
                 list_item.setArt({'thumb': videos['thumbnail'], 'icon': videos['thumbnail'], 'fanart': videos['thumbnail']})
                 list_item.setProperty('IsPlayable', 'true')
@@ -87,6 +91,8 @@ class VideoHandler(object):
                 is_folder = False
                 listing.append((url, list_item, is_folder))
 
+        # Set Content
+        xbmcplugin.setContent(self.handle, 'episodes')
         # Add our listing to Kodi.
         xbmcplugin.addDirectoryItems(self.handle, listing, len(listing))
         # Add a sort method for the virtual folder items
