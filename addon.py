882865b11daddf1634560dcf157ffeeed3ad154b
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-02-05 19:04:23 +0100

    changes

--- addon.py
+++ addon.py
@@ -4,7 +4,7 @@ from audio import AudioHandler
 
 import sys
 import xbmcaddon
-
+import xbmc
 
 addon = xbmcaddon.Addon()
 
@@ -29,44 +29,56 @@ def router(paramstring):
     params = dict(parse_qsl(paramstring))
     # Check the parameters passed to the plugin
     if params:
-        if 'content_type' in params:
-            if params['content_type'] == 'video':
+        if "content_type" in params:
+            if params["content_type"] == "video":
                 vh.list_menu()
                 # vh.list_programs()
-            elif params['content_type'] == 'audio':
-                ah.list_radio_programs()
+            elif params["content_type"] == "audio":
+                ah.list_radios()
+                # ah.list_radio_programs()
         else:
-            if 'action' in params:
-                if params['action'] == 'videomenu':
+            if "action" in params:
+                if params["action"] == "videomenu":
                     # Display the list of videos in a provided category.
-                    if params['option'] == 'all_tvshows':
+                    if params["option"] == "all_tvshows":
                         vh.list_programs()
-                    elif params['option'] == 'last_tvshows':
-                        vh.list_last_broadcast()
-                    elif params['option'] == 'program-type-list':
+                    # elif params['option'] == 'last_tvshows':
+                    #     vh.list_last_broadcast()
+                    elif params["option"] == "program-type-list":
                         vh.list_programs_types()
-                elif params['action'] == 'videotypelisting':
+                elif params["action"] == "videotypelisting":
                     # Display the list of videos in a provided category.
-                    vh.list_programs_types_playlist(params['program'])
-                elif params['action'] == 'videolisting':
+                    vh.list_programs_types_playlist(params["program"])
+                elif params["action"] == "videolisting":
                     # Display the list of videos in a provided category.
-                    vh.list_episodes(params['program'])
-                elif params['action'] == 'videoepisode':
+                    vh.list_playlists(params["program"])
+                    # vh.list_episodes(params['program'])
+                elif params["action"] == "videoprogram":
+                    # Display the list of videos in a provided program.
+                    vh.list_episodes(params["program"])
+                elif params["action"] == "videoepisode":
                     # Display the list of videos in a provided category.
-                    vh.list_videos(params['program'])
-                elif params['action'] == 'videoplay':
+                    vh.list_videos(params["program"])
+                elif params["action"] == "videoplay":
                     # Play a video from a provided URL.
-                    vh.play_video(params['video'])
-                elif params['action'] == 'audioplay':
-                    ah.play_audio(params['program'])
-                elif params['action'] == 'audiolisting':
-                    ah.list_program_audios(params['program'])
+                    vh.play_video(params["video"])
+                elif params["action"] == "audioplay":
+                    ah.play_audio(params["program"])
+                elif params["action"] == "radio":
+                    ah.list_radio_programs(params["program"])
+                elif params["action"] == "audioprogram":
+                    ah.list_seasons(params["program"])
+                elif params["action"] == "audioseason":
+                    ah.list_program_season_chapters(params["program"])
+                elif params["action"] == "audiochapter":
+                    ah.list_program_audios(params["program"])
     else:
         # If the plugin is called from Kodi UI without any parameters,
         # display the list of video categories
         vh.list_programs()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Call the router function and pass the plugin call parameters to it.
     # We use string slicing to trim the leading '?' from the plugin call paramstring
     router(sys.argv[2][1:])
--- audio.py
+++ audio.py
@@ -1,6 +1,8 @@
 from utils import get_radio_programs
 from utils import get_program_audios
-
+from utils import get_radios
+from utils import get_program_seasons
+from utils import get_program_season_chapters
 import xbmcgui
 import xbmcplugin
 
@@ -10,15 +12,14 @@ class AudioHandler(object):
         self.handle = handle
         self.url = url
 
-    def list_radio_programs(self):
+    def list_radios(self):
         listing = []
-        for program in get_radio_programs():
-            title = program.get('title')
-            radio = program.get('radio')
-            program_url = program.get('@id')
+        for radio in get_radios():
+            title = radio.get("title")
+            radio_url = radio.get("@id")
             list_item = xbmcgui.ListItem(label=title)
-            list_item.setInfo('music', {'title': title, 'album': radio})
-            url = '{0}?action=audiolisting&program={1}'.format(self.url, program_url)
+            list_item.setInfo("music", {"title": title, "album": title})
+            url = "{0}?action=radio&program={1}".format(self.url, radio_url)
             is_folder = True
             listing.append((url, list_item, is_folder))
 
@@ -28,20 +29,74 @@ class AudioHandler(object):
         xbmcplugin.addSortMethod(self.handle, xbmcplugin.SORT_METHOD_NONE)
         xbmcplugin.endOfDirectory(self.handle)
 
-    def list_program_audios(self, program):
-        audios = get_program_audios(program)
-        song_list = []
-        for audio in audios:
-            date = audio.get('date')
-            title = audio.get('title')
-            audio_url = audio.get('url')
-            label = u'{0} ({1})'.format(date, title)
-            # create a list item using the song filename for the label
+    def list_radio_programs(self, station):
+        listing = []
+        for program in get_radio_programs(station):
+            title = program.get("title")
+            radio = program.get("radio")
+            program_url = program.get("@id")
+            list_item = xbmcgui.ListItem(label=title)
+            list_item.setInfo("music", {"title": title, "album": radio})
+            url = "{0}?action=audioprogram&program={1}".format(
+                self.url, program_url
+            )
+            is_folder = True
+            listing.append((url, list_item, is_folder))
+
+        # Add our listing to Kodi.
+        xbmcplugin.addDirectoryItems(self.handle, listing, len(listing))
+        # Add a sort method for the virtual folder items
+        xbmcplugin.addSortMethod(self.handle, xbmcplugin.SORT_METHOD_NONE)
+        xbmcplugin.endOfDirectory(self.handle)
+
+    def list_seasons(self, program):
+        seasons = get_program_seasons(program)
+        items = []
+        for season in seasons:
+            label = season.get("title")
+            url = season.get("@id")
             list_item = xbmcgui.ListItem(label=label)
-            list_item.setProperty('IsPlayable', 'true')
-            url = '{0}?action=audioplay&program={1}'.format(self.url, audio_url)
-            is_folder = False
-            song_list.append((url, list_item, is_folder))
+            url = "{0}?action=audioseason&program={1}".format(self.url, url)
+            is_folder = True
+            items.append((url, list_item, is_folder))
+
+        # Add our listing to Kodi.
+        xbmcplugin.addDirectoryItems(self.handle, items, len(items))
+        # Add a sort method for the virtual folder items
+        xbmcplugin.addSortMethod(self.handle, xbmcplugin.SORT_METHOD_NONE)
+        xbmcplugin.endOfDirectory(self.handle)
+
+    def list_program_season_chapters(self, season):
+        chapters = get_program_season_chapters(season)
+        items = []
+        for chapter in chapters:
+            label = chapter.get("title")
+            url = chapter.get("@id")
+            list_item = xbmcgui.ListItem(label=label)
+            url = "{0}?action=audiochapter&program={1}".format(self.url, url)
+            is_folder = True
+            items.append((url, list_item, is_folder))
+
+        # Add our listing to Kodi.
+        xbmcplugin.addDirectoryItems(self.handle, items, len(items))
+        # Add a sort method for the virtual folder items
+        xbmcplugin.addSortMethod(self.handle, xbmcplugin.SORT_METHOD_NONE)
+        xbmcplugin.endOfDirectory(self.handle)
+
+    def list_program_audios(self, program):
+        audio = get_program_audios(program)
+        song_list = []
+
+        date = audio.get("date")
+        title = audio.get("title")
+        audio_url = audio.get("url")
+        label = u"{0} ({1})".format(date, title)
+        # create a list item using the song filename for the label
+        list_item = xbmcgui.ListItem(label=label)
+
+        url = "{0}?action=audioplay&program={1}".format(self.url, audio_url)
+        is_folder = False
+        song_list.append((url, list_item, is_folder))
 
         # Add our listing to Kodi.
         xbmcplugin.addDirectoryItems(self.handle, song_list, len(song_list))
